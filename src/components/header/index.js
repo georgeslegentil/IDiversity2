f66e2d4a05b791afab53a5d5815274cd47eeca01
Dehez Nicolas <nicolas.dehez@epitech.eu>
2021-03-03 12:23:26 +0100

    [FIX] headers problems V1.1

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -64,38 +64,6 @@ Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps 
   );
 };
 
-Header.Profile = function HeaderProfile({ children, ...restProps }) {
-  return <Profile {...restProps}>{children}</Profile>;
-};
-
-Header.Feature = function HeaderFeature({ children, ...restProps }) {
-  return <Feature>{children}</Feature>;
-};
-
-Header.Picture = function HeaderPicture({ src, ...restProps }) {
-  return <Picture {...restProps} src={`/images/users/3.png`} />;
-};
-
-Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
-  return <Dropdown {...restProps}>{children}</Dropdown>;
-};
-
-Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
-  return <Link {...restProps}>{children}</Link>;
-};
-
-Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
-  return <PlayButton {...restProps}>{children}</PlayButton>;
-};
-
-Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
-  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
-};
-
-Header.Text = function HeaderText({ children, ...restProps }) {
-  return <Text {...restProps}>{children}</Text>;
-};
-
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
 };
